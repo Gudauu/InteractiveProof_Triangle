15477d66518d971b66c877fca85c0ecabcb3a170
Gudauu <gudautd@gmail.com>
2024-03-10 19:53:02 -0400

    new project

---
+++ project/src/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+
+using std::vector;
+
+
+
+
+
+class IP{
+
+public:
+    vector<vector<int>>
+
+}
+
+int main(int argc, char *argv[])
+{
+    std::cout << "Hello world!" << std::endl;
+}
